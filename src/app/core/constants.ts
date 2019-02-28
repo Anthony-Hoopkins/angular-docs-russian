2c4b37151d6553c95ce18fe36738f89336dd9a53
Alexey Verbitsky <leshaverbitsky@gmail.com>
2019-02-28 18:02:45 +0300

    feat(app): add menu item strings

---
+++ src/app/core/constants.ts
@@ -0,0 +1,498 @@
+export const MENU_ITEMS = {
+
+  // GETTING STARTED
+  GettingStarted: {
+    menu: 'Getting Started',
+  },
+
+  // TUTORIAL
+  Tutorial: {
+    menu: 'Tutorial',
+    pages: {
+
+      Introduction: {
+        menu: 'Introduction',
+        heading: 'Introduction',
+      },
+      TheApplicationShell: {
+        menu: 'The Application Shell',
+        heading: '',
+      },
+      TheHeroEditor: {
+        menu: '1. The Hero Editor',
+        heading: '',
+      },
+      DisplayingAList: {
+        menu: '2. Displaying a List',
+        heading: '',
+      },
+      MasterDetailComponents: {
+        menu: '3. Master/Detail Components',
+        heading: '',
+      },
+      Services: {
+        menu: '4. Services',
+        heading: '',
+      },
+      Routing: {
+        menu: '5. Routing',
+        heading: '',
+      },
+      HTTP: {
+        menu: '6. HTTP',
+        heading: '',
+      },
+
+    },
+  },
+
+  // FUNDAMENTALS
+  Fundamentals: {
+    menu: 'Fundamentals',
+    pages: {
+
+      Architecture: {
+        menu: 'Architecture',
+        pages: {
+
+          ArchitectureOverview: {
+            menu: 'Architecture Overview',
+            heading: '',
+          },
+          IntroToModules: {
+            menu: 'Intro to Modules',
+            heading: '',
+          },
+          IntroToComponents: {
+            menu: 'Intro to Components',
+            heading: '',
+          },
+          IntroToServicesAndDI: {
+            menu: 'Intro to Services and DI',
+            heading: '',
+          },
+          NextSteps: {
+            menu: 'Next Steps',
+            heading: '',
+          },
+
+        },
+      },
+      ComponentsAndTemplates: {
+        menu: 'Components & Templates',
+        pages: {
+
+          DisplayingData: {
+            menu: 'DisplayingData',
+            heading: '',
+          },
+          TemplateSyntax: {
+            menu: 'Template Syntax',
+            heading: '',
+          },
+          UserInput: {
+            menu: 'User Input',
+            heading: '',
+          },
+          LifecycleHooks: {
+            menu: 'Lifecycle Hooks',
+            heading: '',
+          },
+          ComponentInteraction: {
+            menu: 'Component Interaction',
+            heading: '',
+          },
+          ComponentStyles: {
+            menu: 'Component Styles',
+            heading: '',
+          },
+          AngularElements: {
+            menu: 'Angular Elements',
+            heading: '',
+          },
+          DynamicComponents: {
+            menu: 'Dynamic Components',
+            heading: '',
+          },
+          AttributeDirectives: {
+            menu: 'Attribute Directives',
+            heading: '',
+          },
+          StructuralDirectives: {
+            menu: 'Structural Directives',
+            heading: '',
+          },
+          Pipes: {
+            menu: 'Pipes',
+            heading: '',
+          },
+
+        },
+
+
+      },
+      Forms: {
+        menu: 'Forms',
+        pages: {
+
+          Introduction: {
+            menu: 'Introduction',
+            heading: '',
+          },
+          ReactiveForms: {
+            menu: 'Reactive Forms',
+            heading: '',
+          },
+          TemplateDrivenForms: {
+            menu: 'Template-driven Forms',
+            heading: '',
+          },
+          FormValidation: {
+            menu: 'Form Validation',
+            heading: '',
+          },
+          DynamicForms: {
+            menu: 'Dynamic Forms',
+            heading: '',
+          },
+
+        },
+      },
+      ObservablesAndRxJS: {
+        menu: 'Observables & RxJS',
+        pages: {
+
+          Observables: {
+            menu: 'Observables',
+            heading: '',
+          },
+          TheRxJSLibrary: {
+            menu: 'The RxJS Library',
+            heading: '',
+          },
+          ObservablesInAngular: {
+            menu: 'Observables in Angular',
+            heading: '',
+          },
+          PracticalUsage: {
+            menu: 'Practical Usage',
+            heading: '',
+          },
+          CompareToOtherTechniques: {
+            menu: 'Compare to Other Techniques',
+            heading: '',
+          },
+
+        },
+      },
+      Bootstrapping: {
+        menu: 'Bootstrapping',
+        heading: '',
+      },
+      NgModules: {
+        menu: 'NgModules',
+        pages: {
+
+          NgModulesIntroduction: {
+            menu: 'NgModules Introduction',
+            heading: '',
+          },
+          JSModulesAndNgModules: {
+            menu: 'JS Modules vs NgModules',
+            heading: '',
+          },
+          FrequentlyUsedNgModules: {
+            menu: 'Frequently Used NgModules',
+            heading: '',
+          },
+          TypesOfFeatureModules: {
+            menu: 'Types of Feature Modules',
+            heading: '',
+          },
+          EntryComponents: {
+            menu: 'Entry Components',
+            heading: '',
+          },
+          FeatureModules: {
+            menu: 'Feature Modules',
+            heading: '',
+          },
+          Providers: {
+            menu: 'Providers',
+            heading: '',
+          },
+          SingletonServices: {
+            menu: 'Singleton Services',
+            heading: '',
+          },
+          LazyLoadingFeatureModules: {
+            menu: 'Lazy Loading Feature Modules',
+            heading: '',
+          },
+          SharingNgModules: {
+            menu: 'Sharing Modules',
+            heading: '',
+          },
+          NgModuleAPI: {
+            menu: 'NgModule API',
+            heading: '',
+          },
+          NgModuleFAQ: {
+            menu: 'NgModule FAQ',
+            heading: '',
+          },
+
+        },
+      },
+      DependencyInjection: {
+        menu: 'Dependency Injection',
+        pages: {
+
+          AngularDependencyInjection: {
+            menu: 'Angular Dependency Injection',
+            heading: '',
+          },
+          HierarchicalInjectors: {
+            menu: 'Hierarchical Injectors',
+            heading: '',
+          },
+          DIProviders: {
+            menu: 'DI Providers',
+            heading: '',
+          },
+          DIInAction: {
+            menu: 'DI in Action',
+            heading: '',
+          },
+          NavigateTheComponentTree: {
+            menu: 'Navigate the Component Tree',
+            heading: '',
+          },
+
+        },
+      },
+      HttpClient: {
+        menu: 'HttpClient',
+        heading: '',
+      },
+      RoutingNavigation: {
+        menu: 'Routing & Navigation',
+        heading: '',
+      },
+      Animations: {
+        menu: 'Animations',
+        pages: {
+
+          Introduction: {
+            menu: 'Introduction',
+            heading: '',
+          },
+          TransitionAndTriggers: {
+            menu: 'Transition and Triggers',
+            heading: '',
+          },
+          ComplexSequences: {
+            menu: 'Complex Sequences',
+            heading: '',
+          },
+          ReusableAnimations: {
+            menu: 'Reusable Animation',
+            heading: '',
+          },
+          RouteTransitionAnimations: {
+            menu: 'Route Transition Animations',
+            heading: '',
+          },
+
+        },
+      },
+
+    },
+  },
+
+  // TECHNIQUES
+  Techniques: {
+    menu: 'Techniques',
+    pages: {
+
+      Security: {
+        menu: 'Security',
+        heading: '',
+      },
+      Internationalization: {
+        menu: 'Internationalization (i18n)',
+        heading: '',
+      },
+      ServiceWorkersAndPWA: {
+        menu: 'Service Workers & PWA',
+        pages: {
+
+          Introduction: {
+            menu: 'Introduction',
+            heading: '',
+          },
+          GettingStarted: {
+            menu: 'Getting Started',
+            heading: '',
+          },
+          ServiceWorkerCommunication: {
+            menu: 'Service Worker Communication',
+            heading: '',
+          },
+          ServiceWorkerInProduction: {
+            menu: 'Service Worker in Production',
+            heading: '',
+          },
+          ServiceWorkerConfiguration: {
+            menu: 'Service Worker Configuration',
+            heading: '',
+          },
+
+        },
+      },
+      ServerSideRendering: {
+        menu: 'Server-side Rendering',
+        heading: '',
+      },
+      UpgradingFromAngularJS: {
+        menu: 'Upgrading from AngularJS',
+        pages: {
+
+          UpgradingInstructions: {
+            menu: 'Upgrading Instruction',
+            heading: '',
+          },
+          UpgradingForPerformance: {
+            menu: 'Upgrading for Performance',
+            heading: '',
+          },
+          AngularJSAngularConcepts: {
+            menu: 'AngularJS-Angular Concepts',
+            heading: '',
+          },
+
+        },
+      },
+      AngularLibraries: {
+        menu: 'Angular Libraries',
+        pages: {
+
+          LibrariesOverview: {
+            menu: 'Libraries Overview',
+            heading: '',
+          },
+          UsingPublishedLibraries: {
+            menu: 'Using Published Libraries',
+            heading: '',
+          },
+          CreatingLibraries: {
+            menu: 'Creating Libraries',
+            heading: '',
+          },
+
+        },
+      },
+      AngularIvy: {
+        menu: 'Angular Ivy',
+        heading: '',
+      },
+
+    },
+
+  },
+
+  // SETUP & DEPLOYMENT
+  SetupAndDeployment: {
+    menu: 'Setup & Deployment',
+    pages: {
+
+      ProjectFileStructure: {
+        menu: 'Project File Structure',
+        heading: '',
+      },
+      WorkspaceConfiguration: {
+        menu: 'Workspace Configuration',
+        heading: '',
+      },
+      NpmDependencies: {
+        menu: 'npm Dependencies',
+        heading: '',
+      },
+      TypeScriptConfiguration: {
+        menu: 'TypeScript Configuration',
+        heading: '',
+      },
+      AheadOfTimeCompilation: {
+        menu: 'Ahead-of-Time Compilation',
+        heading: '',
+      },
+      BuildingAndServing: {
+        menu: 'Building & Serving',
+        heading: '',
+      },
+      Testing: {
+        menu: 'Testing',
+        heading: '',
+      },
+      Deployment: {
+        menu: 'Deployment',
+        heading: '',
+      },
+      BrowserSupport: {
+        menu: 'Browser Support',
+        heading: '',
+      },
+      DevToolIntegration: {
+        menu: 'Dev Tool Integration',
+        pages: {
+
+          LanguageService: {
+            menu: 'Language Service',
+            heading: '',
+          },
+
+        },
+      },
+
+    },
+  },
+
+  // RELEASE INFORMATION
+  ReleaseInformation: {
+    menu: 'Release Information',
+    pages: {
+
+      KeepingUpToDate: {
+        menu: 'Keeping Up-to-Date',
+        heading: '',
+      },
+      AngularReleases: {
+        menu: 'Angular Releases',
+        heading: '',
+      },
+
+    },
+  },
+
+  // QUICK REFERENCE
+  QuickReference: {
+    menu: 'Quick Reference',
+    pages: {
+
+      CheatSheet: {
+        menu: 'Cheat Sheet',
+        heading: '',
+      },
+      StyleGuide: {
+        menu: 'Style Guide',
+        heading: '',
+      },
+      Glossary: {
+        menu: 'Glossary',
+        heading: '',
+      },
+
+    },
+  },
+
+};
